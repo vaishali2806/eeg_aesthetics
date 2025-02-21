13b2d67e549746ebc03dfdc52cb528870efecb01
Vaishali Goyal <36100198+vaishali2806@users.noreply.github.com>
2025-02-21 16:32:44 +0100

    Update generate_mask_indices.m

--- RSVP Task/generate_mask_indices.m
+++ RSVP Task/generate_mask_indices.m
@@ -1,5 +1,5 @@
 
-function data = generate_mask_indices(dat, unique_ent)
+function data = generate_mask_indices(dat, unique_ent, checkfaster)
 
 length_vec = length(dat.new_stim)
 % Set the length of the vector and number of unique entries
@@ -7,13 +7,17 @@ vector_length = length_vec;
 unique_entries = unique_ent;
 
 % Generate a vector with equal entries for each number from 1 to 20
-random_vector = repmat(1:unique_entries, 1, round(vector_length / unique_entries));
+if (checkfaster == 1)
+    random_vector = repmat(1:unique_entries, 1, round(vector_length / unique_entries) + 1);
+else
+    random_vector = repmat(1:unique_entries, 1, round(vector_length / unique_entries));
+end 
 
 % Shuffle the vector to randomize the order
 random_vector = random_vector(randperm(length(random_vector)));
 
 % Display the generated vector
-disp(random_vector);
+% disp(random_vector);
 dat.mask_indices = random_vector;
 data = dat
 
